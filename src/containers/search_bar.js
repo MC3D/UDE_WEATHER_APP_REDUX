53366a73ce2ae1983547cfbff1443642c13809f3
MC3D <m.chapman.home@gmail.com>
2016-11-29 12:22:26 -0500

    refactors SearchBar as a container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCretors } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,8 +16,8 @@ export default class SearchBar extends Component {
   }
   _handleSubmit(event){
     event.preventDefault();
-    
-    // need to fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
   render() {
     return (
@@ -32,3 +35,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
