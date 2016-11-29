96b02a7e97b0f32ce1256a2cf5aed1e9bae7070d
MC3D <m.chapman.home@gmail.com>
2016-11-29 11:07:28 -0500

    transforms SearchBar into a controlled component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+    this._handleInput = this._handleInput.bind(this);
+  }
+  _handleInput(event){
+    this.setState({term: event.target.value});
+  }
   render() {
     return (
       <form className='input-group'>
-        <input />
+        <input
+          placeholder='Get a five-day forecast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this._handleInput}
+         />
         <span className='input-group-btn'>
           <button className='btn btn-secondary' type='submit'>SUBMIT</button>
         </span>
