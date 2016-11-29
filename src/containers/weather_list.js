729772cdc85f561dc017d6a8abf1b2417bfc5ec9
MC3D <m.chapman.home@gmail.com>
2016-11-29 13:40:16 -0500

    makes reusable chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
 
 class WeatherList extends Component {
   _renderWeather(cityData) {
-    const name = cityData.city.name
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td><Chart data={temps} color='orange' /></td>
+        <td><Chart data={pressure} color='blue' /></td>
+        <td><Chart data={temps} color='green' /></td>
       </tr>
     );
   }
